c90436d5f002616c26e8f3a4b6b29dd4c90735ab
Paul Crane <paul@acheronsoft.com>
2022-09-18 23:36:47 -0500

    Add initial functions fetchWord and LookupWord

--- pkg/slang/slang.go
+++ pkg/slang/slang.go
@@ -1,1 +1,9 @@
 package slang
+
+func fetchWord(wordToFind string) (string, error) {
+	return "Hello World", nil
+}
+
+func LookupWord(wordToFind string) (string, error) {
+	return fetchWord(wordToFind)
+}
