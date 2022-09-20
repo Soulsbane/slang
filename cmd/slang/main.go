38a5b47fdd3ec1cdc66baef70b5e792a09486596
Paul Crane <paul@acheronsoft.com>
2022-09-19 22:25:45 -0500

    Add support for looking up first definition to a passed argument

--- cmd/slang/main.go
+++ cmd/slang/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/Soulsbane/slang/pkg/slang"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		return
+	}
+
+	wordToFind := args[0]
+	definition, err := slang.LookupWord(wordToFind)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(definition)
 }
--- pkg/slang/slang.go
+++ pkg/slang/slang.go
@@ -1,7 +1,38 @@
 package slang
 
+import (
+	"github.com/imroc/req/v3"
+)
+
+const API_URL = "http://api.urbandictionary.com/v0/define"
+
+type SearchResult struct {
+	Type    string `json:"result_type"`
+	Tags    []string
+	Results []Result `json:"list"`
+	Sounds  []string
+}
+
+type Result struct {
+	Author     string
+	Word       string
+	Definition string
+	Example    string
+	Permalink  string
+	Upvote     int `json:"thumbs_up"`
+	Downvote   int `json:"thumbs_down"`
+}
+
 func fetchWord(wordToFind string) (string, error) {
-	return "Hello World", nil
+	client := req.C()
+	var result SearchResult
+
+	_, err := client.R().SetQueryParam("term", wordToFind).SetResult(&result).Get(API_URL)
+
+	if err != nil {
+	}
+
+	return result.Results[0].Definition, err
 }
 
 func LookupWord(wordToFind string) (string, error) {
