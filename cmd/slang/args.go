f5e7a68c2581d07c37ad81010035f46fb5d742bc
Paul Crane <paul@acheronsoft.com>
2022-12-11 13:00:50 -0600

    Add description to CLI output

--- cmd/slang/args.go
+++ cmd/slang/args.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/carlmjohnson/versioninfo"
+import (
+	"github.com/carlmjohnson/versioninfo"
+)
 
 type ProgramArgs struct {
 	Word    string `arg:"positional, required"`
 	ListAll bool   `arg:"-a,--list-all" default:"false"`
+}
+
+func (args ProgramArgs) Description() string {
+	return "Gets the definition of a slang word from Urban Dictionary"
 }
 
 func (ProgramArgs) Version() string {
