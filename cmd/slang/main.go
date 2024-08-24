28959d80eda249d4e4515d861355a75d8b7f04af
Paul Crane <paul@acheronsoft.com>
2024-08-23 22:19:08 -0500

    Add table output code. Note it is not enabled currently
    The table renders fine on one definition but multiplee definitions
    completely breaks the table output and requires more investigation
    before fully enabling it.

--- cmd/slang/main.go
+++ cmd/slang/main.go
@@ -5,8 +5,22 @@ import (
 	"fmt"
 	"github.com/Soulsbane/slang/pkg/slang"
 	"github.com/alexflint/go-arg"
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/tiagomelo/go-clipboard/clipboard"
+	"os"
 )
+
+func getOutputTable() table.Writer {
+
+	outputTable := table.NewWriter()
+
+	outputTable.SetOutputMirror(os.Stdout)
+	outputTable.AppendHeader(table.Row{"Definition"})
+	outputTable.SetStyle(table.StyleRounded)
+	outputTable.Style().Options.SeparateRows = true
+
+	return outputTable
+}
 
 func handleCopyToClipboard(definition string) {
 	c := clipboard.New()
@@ -17,17 +31,23 @@ func handleCopyToClipboard(definition string) {
 }
 
 func handleListResults(results []slang.Result, listAll bool, copyToClipboard bool) {
+	//outputTable := getOutputTable()
+
 	if listAll {
 		for _, result := range results {
+			//outputTable.AppendRow(table.Row{result.Definition})
 			fmt.Println(result.Definition + "\n")
 		}
 	} else {
+		//outputTable.AppendRow(table.Row{results[0].Definition})
 		fmt.Println(results[0].Definition)
 
 		if copyToClipboard {
 			handleCopyToClipboard(results[0].Definition)
 		}
 	}
+
+	//outputTable.Render()
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -19,12 +19,15 @@ require (
 	github.com/google/pprof v0.0.0-20240416155748-26353dc0451f // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.5.9 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/onsi/ginkgo/v2 v2.17.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/quic-go/qpack v0.4.0 // indirect
 	github.com/quic-go/quic-go v0.42.0 // indirect
 	github.com/refraction-networking/utls v1.6.4 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	go.uber.org/mock v0.4.0 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
